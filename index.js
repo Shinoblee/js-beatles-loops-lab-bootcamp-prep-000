a2e1e9537766e409b4453adf25a02858b3bba1ae
Lee Simon <shinoblee@gmail.com>
2018-08-06 20:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ function iLoveTheBeatles(num){
   do {
     arr.push("I love the Beatles");
     num++;
-  }while (num < 15){
-    arr.push("I love the Beatles");
-  }
+  }while (num < 15);
+  
   return arr;
 }
